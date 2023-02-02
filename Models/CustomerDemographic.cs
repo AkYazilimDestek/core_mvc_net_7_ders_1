62b13ba11c572dbcf32cbd5ff75570c020d4b88e
AHMET AKIN <akinahmed277@gmail.com>
2023-02-01 21:02:06 -0800

    proje olustu

---
+++ Models/CustomerDemographic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Web_one.Models;
+
+public partial class CustomerDemographic
+{
+    public string CustomerTypeId { get; set; } = null!;
+
+    public string? CustomerDesc { get; set; }
+
+    public virtual ICollection<Customer> Customers { get; } = new List<Customer>();
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Web_one.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
